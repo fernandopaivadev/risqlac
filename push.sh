e0568bfa3a2f91e69f96d9bafb8fbeb4fbefa0e2
Fernando Paiva <contato@fernandopaiva.dev>
2024-03-14 13:00:20 -0300

    implementa build da imagem docker para várias arquiteturas

--- push.sh
+++ push.sh
@@ -4,11 +4,5 @@ USERNAME="fernandopaivadev"
 echo "==> delete previous images"
 sudo docker rmi $APP_NAME $USERNAME/$APP_NAME
 
-echo "==> build image"
-sudo docker build -t $APP_NAME .
-
-echo "==> tag image"
-sudo docker tag $APP_NAME $USERNAME/$APP_NAME:latest
-
-echo "==> push image"
-sudo docker push $USERNAME/$APP_NAME
+echo "==> build, tag and push image"
+sudo docker buildx build --platform linux/amd64,linux/arm/v7 -t $USERNAME/$APP_NAME:latest --push .
